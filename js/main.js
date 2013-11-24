b61949e10c8a530c6f2eb3d890214489d7bf974f
Zachary Liu <liu.zachary@gmail.com>
2013-11-24 18:04:48 -0500

    Page layout, cookie display, and cookie click score.

---
+++ js/main.js
@@ -0,0 +1,35 @@
+(function() {
+    var score = 0;
+    var scoreLastSecond = 0;
+    function scorePlusOne() {
+        score += 1;
+        scoreLastSecond += 1;
+        scoreUpdate();
+    }
+    function scoreUpdate() {
+        $('#clicker-score-num').text(score);
+    }
+    function scoreReset() {
+        score = 0;
+        scoreUpdate();
+    }
+    function cpsUpdate() {
+        $('#clicker-cps-num').text(scoreLastSecond);
+        scoreLastSecond = 0;
+    }
+    function init() {
+        scoreUpdate();
+        setInterval(function(){
+            cpsUpdate()
+        }, 1000);
+
+        // click handlers
+        $('#clicker-cookie').click(function(e){
+            e.preventDefault();
+            scorePlusOne();
+        })
+    }
+    $(function() {
+        init();
+    })
+})();
