aadd12983001602581083dfecde5cd77be69ffa8
Zachary Liu <liu.zachary@gmail.com>
2013-11-25 01:56:30 -0500

    Replace SQLite code with simpler direct .csv file writing.

--- submit.php
+++ submit.php
@@ -1,30 +1,9 @@
 <?php
-$dbhandle = sqlite_open('data.db', 0666, $error);
-if (!$dbhandle) die ($error);
+$filename = 'data.csv';
 
-if (isset($_GET['create']) && $_GET['create'] = 'yes') {
-    $query = "CREATE TABLE scores(id INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL, email TEXT NOT NULL, score INT NOT NULL) ;";
-    if (!sqlite_exec($dbhandle, $query)) {
-        die('Could not create.');
-    };
-    echo "Successfully created database.";
-} else if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['score'])) {
-    $name = sqlite_escape_string($_POST['name']);
-    $email = sqlite_escape_string($_POST['email']);
-    $score = intval($_POST['score']);
-    $query = "INSERT INTO scores (name, email, score) VALUES ('$name', '$email', $score)";
-    if (!sqlite_exec($dbhandle, $query)) {
-        die('Could not save.');
-    };
-    echo "Success";
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['score'])) {
+    $data = $_POST['name'].','.$_POST['email'].','.$_POST['score']."\n"; // double quotes needed to get newline
+    file_put_contents($filename, $data, FILE_APPEND | LOCK_EX);
 } else {
-    $query = "SELECT * FROM scores";
-    $result = sqlite_query($dbhandle, $query);
-    if (!$result) die("Cannot execute query.");
-    while ($row = sqlite_fetch_array($result, SQLITE_ASSOC)) {
-        var_dump($row);
-        echo "<br>";
-    }
+    die('Data not specified');
 }
-
-sqlite_close($dbhandle);
