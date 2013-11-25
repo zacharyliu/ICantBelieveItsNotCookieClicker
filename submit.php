64f03894ca3efa3bcc2f2de39b4fbfc015cd4807
Zachary Liu <liu.zachary@gmail.com>
2013-11-25 04:06:47 -0500

    Record time with score save.

--- submit.php
+++ submit.php
@@ -2,7 +2,7 @@
 $filename = 'data.csv';
 
 if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['score'])) {
-    $data = $_POST['name'].','.$_POST['email'].','.$_POST['score']."\n"; // double quotes needed to get newline
+    $data = date('D M j Y g:i:s A T') . ',' . $_POST['name'].','.$_POST['email'].','.$_POST['score']."\n"; // double quotes needed to get newline
     file_put_contents($filename, $data, FILE_APPEND | LOCK_EX);
 } else {
     die('Data not specified');
